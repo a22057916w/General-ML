a3db8d10d1e8e599b0fdc29d44da5bc201334d42
a22057916w <a22057916w@gmail.com>
2021-07-15 00:09:00 +0800

    def class LinearRegression()

--- linear_regression/imp.py
+++ linear_regression/imp.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+class LinearRegression():
+    def __init__(self, num_iteration=100, learning_rate=1e-1, feature_scaling=True):
+        self.num_iteration = num_iteration
+        self.learning_rate = learning_rate
+        self.feature_scaling = feature_scaling
+        self.M = 0 # normalize mean
+        self.S = 1 # normalize range
+        self.W = None
+        self.cost_history = np.empty(num_iteration)
