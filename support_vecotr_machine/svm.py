aeca3285ccb9029041095d8629aa0d3772e77a97
a22057916w <a22057916w@gmail.com>
2021-08-30 16:57:54 +0800

    deving svm

--- support_vecotr_machine/svm.py
+++ support_vecotr_machine/svm.py
@@ -3,3 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import datasets
+
+iris = datasets.load_iris()
+for key, value in iris.items():
+    try:
+        print(key, value.shape)
+    except:
+        print(key)
+
+
+x = pd.DataFrame(iris['data'], columns=iris['feature_names'])
+#print(x)
+
+y = pd.DataFrame(iris['target'], columns=['target_names'])
+#print(y)
+
+iris_data = pd.concat([x, y], axis=1)
+print(iris_data)
