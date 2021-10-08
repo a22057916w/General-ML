571531ab83397ec82cf561f2b8866ae1969ff869
a22057916w <a22057916w@gmail.com>
2021-10-08 14:10:46 +0800

    studying svm

--- support_vecotr_machine/svm.py
+++ support_vecotr_machine/svm.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from sklearn import datasets
 
 # ---------------- load iris dataset ---------------------
-print("------ Loading Iris Dataset")
+print("------ Loading Iris Dataset ------")
 # type(iris) return <class 'sklearn.utils.Bunch'>
 iris = datasets.load_iris()
 for key, value in iris.items():
@@ -83,7 +83,7 @@ iris_data = iris_data[["sepal length (cm)", "petal length (cm)", "target"]]
 
 """
 
-#print(iris_data["target"].isin([0, 1])) 
+#print(iris_data["target"].isin([0, 1]))
 """
 [150 rows x 3 columns]
 0       True
@@ -100,6 +100,7 @@ iris_data = iris_data[["sepal length (cm)", "petal length (cm)", "target"]]
 
 """
 
+# assign series to dataframe as key, which the key is the col name of series
 iris_data = iris_data[iris_data["target"].isin([0, 1])]
 #print(iris_data)
 """    sepal length (cm)  petal length (cm)  target
@@ -131,8 +132,94 @@ print("-------------------------------------------------")
 
 
 from sklearn.model_selection import train_test_split
+
+# 將資料分為train與test
 X_train, X_test, y_train, y_test = train_test_split(
     iris_data[['sepal length (cm)','petal length (cm)']], iris_data[['target']], test_size=0.3, random_state=0)
 
-print(X_train, X_test)
-print(y_train, y_test)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+
+from sklearn.svm import SVC
+svm = SVC(kernel='linear', probability=True)
+svm.fit(X_train_std,y_train['target'].values)
+
+
+svm.predict(X_test_std)
+
+
+y_test['target'].values
+
+
+
+error = 0
+for i, v in enumerate(svm.predict(X_test_std)):
+    if v!= y_test['target'].values[i]:
+        error+=1
+print(error)
+
+svm.predict_proba(X_test_std)
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.6,
+                    c=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx],
+                    label=cl)
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
+            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
+            warnings.warn('Please update to NumPy 1.9.0 or newer')
+        else:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    alpha=1.0,
+                    edgecolor='black',
+                    linewidths=1,
+                    marker='o',
+                    s=55, label='test set')
+
+plot_decision_regions(X_train_std, y_train['target'].values, classifier=svm)
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
